6cea52b722a56a54390e707acd3a44e5d67074ba
Sérgio Amorim <saamorim@gmail.com>
2019-09-21 13:24:12 +0100

    Support token or username and password (#2)

--- SonarConfigurator/CmdOptions.cs
+++ SonarConfigurator/CmdOptions.cs
@@ -11,8 +11,14 @@ namespace SonarConfiguration
         [Option('h', "host", Required = true, HelpText = "Host where Sonarqube is running")]
         public string SonarqubeHost { get; set; }
 
-        [Option('t', "token", Required = true, HelpText = "Token access to Sonarqube")]
+        [Option('t', "token", Required = true, HelpText = "Token access to Sonarqube", SetName = "token")]
         public string SonarqubeToken { get; set; }
+
+        [Option('u', "username", Required = true, HelpText = "Username access to Sonarqube", SetName = "userpass")]
+        public string SonarqubeUsername { get; set; }
+
+        [Option('p', "password", Required = true, HelpText = "Password access to Sonarqube", SetName = "userpass")]
+        public string SonarqubePassword { get; set; }
 
         [Option('v', "verbose", Required = false, HelpText = "Set output to verbose messages.")]
         public bool Verbose { get; set; }
